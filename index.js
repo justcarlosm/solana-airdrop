51660a117d18f2fd07de20fd64d3886386340fd5
Carlos Mendoza <kaziel@gmail.com>
2021-12-26 17:18:35 -0600

    added test function

--- index.js
+++ index.js
@@ -9,25 +9,27 @@ const {
    } = require("@solana/web3.js");
 
    const newPair = new Keypair();
+   console.log(newPair);
 
    const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
 
    const secretKey = newPair._keypair.secretKey
 
    const getWalletBalance = async () => {
-    try {
-        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const myWallet = await Keypair.fromSecretKey(secretKey);
-        const walletBalance = await connection.getBalance(
-            new PublicKey(myWallet.publicKey)
-        );
-        console.log(`=> For wallet address ${publicKey}`);
-        console.log(`Wallet balance: ${walletBalance}`);
+        try {
+            const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+            const myWallet = await Keypair.fromSecretKey(secretKey);
+            const walletBalance = await connection.getBalance(
+                new PublicKey(myWallet.publicKey)
+            );
+            console.log(`=> For wallet address ${publicKey}`);
+            console.log(`Wallet balance: ${walletBalance}`);
 
-    } catch (err) {
-        console.log(err);
-    }
-
+        } catch (err) {
+            console.log(err);
+        }
+    };
+ 
     const airDropSol = async () => {
         try {
             const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
@@ -42,4 +44,9 @@ const {
             console.log(err);
         }
     };
-};
+
+    const driverFunction = async () => {
+        await getWalletBalance();
+        await airDropSol();
+        await getWalletBalance();
+    }
